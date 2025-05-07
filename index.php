18e6847246d497408aef8ce2e4cfe1523cbb3f11
Mario <astorgamhdez@gmail.com>
2025-05-07 15:16:04 -0700

    Select wip

--- index.php
+++ index.php
@@ -15,13 +15,14 @@
         name="codigo_postal" 
         required 
         placeholder="Ejemplo: 12345" 
-        pattern="\d{5}" 
+        pattern="[0-9]{5}"
         value="<?php if (isset($_GET['codigo_postal'])) { echo $_GET['codigo_postal']; } ?>">
         <button type="submit">Buscar</button>
     </form>
-    <form action="index.php" method="post">
+    <form action="index.php" method="get">
+        <input type="hidden" name="codigo_postal" value="<?php if (isset($_GET['codigo_postal'])) { echo $_GET['codigo_postal']; } ?>">
         <label for="Colonias">Colonias</label>
-        <select name="cmboxColonias" id="Colonias">
+        <select name="cmboxColonias" id="Colonias" select="<?php if (isset($_GET['cmboxColonias'])) { echo $_GET['cmboxColonias']; } ?>">
             <?php
             include 'libreria.php';
             if (isset($_GET['codigo_postal'])) {
@@ -35,27 +36,17 @@
         <button type="submit">Enviar</button>
     </form>
         <label for="txtMunicipio">Municipio:</label>
-        <output id="txtMunicipio"><?php if (isset($_GET['codigo_postal']) && isset($_GET['cmboxColonias'])) {
-                include 'libreria.php';
-                $conexion = ConexionBD();
-                $codigo_postal = $_POST['codigo_postal'];
-                $colonia = $_POST['cmboxColonias'];
-                $query = "SELECT Municipio FROM codigospostales WHERE Cp = '$codigo_postal' AND Colonia = '$colonia'";
-                $resultado = $conexion->query($query);
-
-                if ($resultado && $resultado->num_rows > 0) {
-                    $fila = $resultado->fetch_assoc();
-                    echo $fila['Municipio'];
-                } else {
-                    echo "No se encontraron resultados.";
-                }
-
-                $conexion->close();
-            } else {
-                echo "Seleccione una colonia para ver el municipio.";
+        <output id="txtMunicipio"><?php 
+            if (isset($_GET['codigo_postal']) && isset($_GET['cmboxColonias'])) {
+                echo BuscarMunicipio($_GET['codigo_postal'], $_GET['cmboxColonias']);
             }
+        ?></output><br>
+        <label for="lblEstado">Estado: </label>
+        <output id="lblEstado"><?php 
+        if (isset($_GET['codigo_postal']) && isset($_GET['cmboxColonias'])) {
+            echo BuscarEstado($_GET['codigo_postal'], $_GET['cmboxColonias']);
+        }
         ?></output>
-        <label for="">Estado</label>
     </div>
 </body>
 </html>
--- libreria.php
+++ libreria.php
@@ -17,11 +17,36 @@ function ObtenerColonias($codigo_postal){
     echo "<option value='' select disabled>Seleccione una colonia</option>";
     if($resultado->num_rows>0){
         while($colonia=$resultado->fetch_assoc()){
-            echo "<option value='".$colonia['Cp']."'>".$colonia['Colonia']."</option>";
+            
+            echo "<option value='".$colonia['Colonia']."'>".$colonia['Colonia']."</option>";
         }
     }
     else{
         echo "<option value=''>No se encontraron resultados</option>";
+    }
+    $conexion->close();
+}
+function BuscarMunicipio($codigo_postal, $colonia){
+    $conexion = ConexionBD();
+    $query = "SELECT Municipio FROM codigospostales WHERE Cp = '$codigo_postal' AND Colonia = '$colonia'";
+    $resultado = $conexion->query($query);
+    if ($resultado && $resultado->num_rows > 0) {
+        $fila = $resultado->fetch_assoc();
+        return $fila['Municipio'];
+    } else {
+        return "No se encontraron resultados.";
+    }
+    $conexion->close();
+}
+function BuscarEstado($codigo_postal, $colonia){
+    $conexion = ConexionBD();
+    $query = "SELECT Estado FROM codigospostales WHERE Cp = '$codigo_postal' AND Colonia = '$colonia'";
+    $resultado = $conexion->query($query);
+    if ($resultado && $resultado->num_rows > 0) {
+        $fila = $resultado->fetch_assoc();
+        return $fila['Estado'];
+    } else {
+        return "No se encontraron resultados.";
     }
     $conexion->close();
 }
